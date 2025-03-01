c69bc1692fc761a2ed835c6843b8a8184d4f8474
ben <benjaminstrange@my.unt.edu>
2025-02-28 18:00:13 -0600

    bootstrap, sass, navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('resources/css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'LSAPP')}}</title>
 
         <!-- Fonts -->
 
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> {{$title}}  </h1>
-    <p> This is the laravel app "from scratch series" </p>
+    <div class="jumbotron">
+        <h1> Welcome to Laravel </h1>
+        <p> This is the laravel app "from scratch series" </p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a><a class="btn btn-primary btn-lg" href="/register" role="button">Register</a></p>
+    </div>
 @endsection
    
