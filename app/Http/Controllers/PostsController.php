2c57377a0878d4c65689de63b2ea75b713c72c7f
ben <benjaminstrange@my.unt.edu>
2025-03-06 12:16:56 -0600

    Added edit and Delete functionality

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -76,6 +76,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
         //
     }
 
@@ -88,7 +90,18 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body'  => 'required'
+        ]);
+        
+        //Create Posts
+        $post = Post::find($id);
+        $post->title = $request->input('title');    
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
@@ -99,6 +112,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
